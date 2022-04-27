ac698a84124d4810199ef020e164c276c6c13aa0
boral <boralsoumya@yahoo.com>
2022-04-27 20:58:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,53 @@
+import bs4 as bs
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import streamlit as st
+
+
+class Helper():
+
+    global browser
+    
+    def init(self):
+        self.API_TOKEN = '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'
+        self.headers = {"Authorization": f"Bearer {self.API_TOKEN}"}
+        self.API_URL = "https://api-inference.huggingface.co/models/deepset/roberta-base-squad2"
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        browser = webdriver.Chrome("C:\\Program Files\\chromedriver.exe",chrome_options=chrome_options)
+
+    def spell_check(self,input_term):
+        self.suggestion = input_term
+
+    def get_snippet(self):
+        self.query = self.suggestion.replace(" ","+")
+        self.url = "https://www.google.com/search?q=" + self.query
+        browser.get(self.url)
+        html_source = browser.page_source
+        browser.close()
+        soup = bs.BeautifulSoup(html_source, "html")
+        text = soup.find('span',class_='hgKElc').text
+        return text
+    
+def get_browser():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    browser = webdriver.Chrome("C:\\Program Files\\chromedriver.exe",chrome_options=chrome_options)
+    return browser
+
+
+st.header( 'Question' )
+
+input_term = st.text_area('','What is the impact of Globalization ?')
+
+if st.button('Ask'):
+    browser = get_browser()
+    helper = Helper()
+    helper.spell_check(input_term)
+    snippet = helper.get_snippet()
+else:
+    snippet = ''
+
+st.header( 'Answer' )
+
+st.markdown( snippet )
